cf31760a6666e21c802ca68e84e980bc14ff2947
CSzacherski <cezaryszacherski@gmail.com>
2023-11-27 18:06:26 +0100

    add new azure functions to get and add visitor

---
+++ backend/CloudResume.FunctionApp/CloudResume.FunctionApp/AddVisitor.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace CloudResume.FunctionApp;
+
+public static class AddVisitor
+{
+    [FunctionName("AddVisitor")]
+    public static async Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Function, "put", Route = "visitors")] HttpRequest req, ILogger log)
+    {
+        return new OkObjectResult($"Add visitor");
+    }
+}
